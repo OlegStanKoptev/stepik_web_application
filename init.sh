1a4fd70947eefab6d818207c49d51a20db290a06
Oleg Koptev <olegstankoptev@gmail.com>
2022-03-15 12:43:44 +0000

    step 1 finished

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.cong /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c hello.py hello:wsgi_app
+cd /home/box/web/ask
+sudo gunicorn --bind=0.0.0.0:8080 ask.wsgi
+cd ../
